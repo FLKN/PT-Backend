1cfcb04b89966b9ac84fc991c4515922bae1e550
David Pérez Espino <dpereze1105@hotmail.com>
2016-11-23 00:35:33 -0600

    Ordenes listas

--- models/dishes.js
+++ models/dishes.js
@@ -1,52 +1,47 @@
 var mysql = require('mysql'),
 
-connection = mysql.createConnection(
-{ 
-	host: 'us-cdbr-azure-southcentral-f.cloudapp.net',
-	user: 'b77bca855ed18d',
-	password: '595352af',
-	database: 'pt_db',
-	port: 3306
-});
- 
+    connection = mysql.createConnection({
+        host: 'us-cdbr-azure-southcentral-f.cloudapp.net',
+        user: 'b77bca855ed18d',
+        password: '595352af',
+        database: 'pt_db',
+        port: 3306
+    });
+
 var dishesModel = {};
 
 
-dishesModel.getDishes = function(callback)
-{
-	var sql = "SELECT * FROM platillos";
-	if (connection) {
-		connection.query(sql, function(error, rows) {
-			if(error) { throw error; }
-			else { callback(null, rows); }
-		});
-	}
+dishesModel.getDishes = function(callback) {
+    var sql = "SELECT * FROM platillos ORDER BY id";
+    if (connection) {
+        connection.query(sql, function(error, rows) {
+            if (error) { throw error; } else { callback(null, rows); }
+        });
+    }
 }
 
 dishesModel.setOrder = function(no_dishes, total_cost, now, room, callback) {
-	var data_insert = {
-		costo_total : total_cost, 
-		cant_platillos : no_dishes,
-		hora_creacion : now,
-		id_habitacion : room
-	};
-	if (connection) {
-		connection.query("INSERT INTO ordens SET ?", data_insert, function(error, rows) {
-			if(error) { throw error; }
-			else { callback(null, rows); }
-		});
-	}
+    var data_insert = {
+        costo_total: total_cost,
+        cant_platillos: no_dishes,
+        hora_creacion: now,
+        id_habitacion: room
+    };
+    if (connection) {
+        connection.query("INSERT INTO ordens SET ?", data_insert, function(error, rows) {
+            if (error) { throw error; } else { callback(null, rows); }
+        });
+    }
 }
 dishesModel.makeOrder = function(id_order, id_dish, callback) {
-	var data_insert = {
-		id_orden : id_order,
-		id_platillo : id_dish
-	};
-	if (connection) {
-		connection.query("INSERT INTO orden_platillos SET ?", data_insert, function(error, rows) {
-			if(error) { throw error; }
-			else { callback(null, rows); }
-		});
-	}	
+    var data_insert = {
+        id_orden: id_order,
+        id_platillo: id_dish
+    };
+    if (connection) {
+        connection.query("INSERT INTO orden_platillos SET ?", data_insert, function(error, rows) {
+            if (error) { throw error; } else { callback(null, rows); }
+        });
+    }
 }
 module.exports = dishesModel;
--- routes/room_service.js
+++ routes/room_service.js
@@ -1,45 +1,57 @@
 var Dish = require('../models/dishes');
 var moment = require("moment");
 
-module.exports = function(app)
-{
-	app.post("/dishes/get_dishes",function(req, res){    
-	    Dish.getDishes(function(error,data) {
-	      	if (data.length == 0)
-	        	res.send({
-	          	action : false, 
-	          	msg : "Cuarto incorrecto"
-        	});
-	      	else{
-				res.send({
-					action : true, 
-					dishes : data,
-					msg : "Acción realizada"
-				});
-			}
-		});
-	});
+module.exports = function(app) {
+    app.post("/dishes/get_dishes", function(req, res) {
+        Dish.getDishes(function(error, data) {
+            if (data.length == 0)
+                res.send({
+                    action: false,
+                    msg: "Cuarto incorrecto"
+                });
+            else {
+                var dishes = [];
+                for (var i = 0; i < data.length; i++) {
+                    dishes.push("{ id: '" + data[i].id + "'" +
+                        ",nombre:'" + data[i].nombre + "'" +
+                        ",precio:'" + data[i].precio + "'" +
+                        //",imagen: " + data[i].imagen +
+                        " }");
+                }
 
-	app.post("/dishes/set_order",function(req, res){
-		var room = req.body.room;
-		var dishes_ids = req.body.dishes;
-		var total_cost = req.body.total;
-		
-		var array_ids = dishes_ids.split(",");
-		for(var i=0; i < array_ids.length; i++) { array_ids[i] = parseInt(array_ids[i], 10); } 
-		
-		var now = moment().add(1,'month').format("YYYY-MM-DD H:mm:ss"); 
+                res.send({
+                    action: true,
+                    dishes: JSON.stringify(dishes),
 
-		Dish.setOrder(array_ids.length, total_cost, now, room, function(error, data) {
-			
-			for(var i=0; i < array_ids.length; i++) { 
-				Dish.makeOrder(data.insertId, array_ids[i], function(error, data) { });
-			} 
 
-			res.send({
-				action : true, 
-				msg : "Orden Creada"
-			});
-		});
-	});
+                    msg: "Acción realizada"
+                });
+            }
+        });
+    });
+
+    app.post("/dishes/set_order", function(req, res) {
+        var room = req.body.room;
+        var dishes_ids = req.body.dishes;
+        var total_cost = req.body.total;
+
+        dishes_ids = dishes_ids.substring(1, dishes_ids.length - 1).split(",");
+
+        for (var i = 0; i < dishes_ids.length; i++) { dishes_ids[i] = parseInt(dishes_ids[i], 10); }
+
+        var now = moment().add(1, 'month').format("YYYY-MM-DD H:mm:ss");
+
+        Dish.setOrder(dishes_ids.length, total_cost, now, room, function(error, data) {
+
+            for (var i = 0; i < dishes_ids.length; i++) {
+                Dish.makeOrder(data.insertId, dishes_ids[i], function(error, data) {});
+            }
+
+            res.send({
+                action: true,
+                msg: "Orden Creada"
+            });
+        });
+
+    });
 }
